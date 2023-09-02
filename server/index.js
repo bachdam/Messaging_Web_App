e549dbf9df0e24311a1d5613c56b94007208561a
bachdam <131636477+bachdam@users.noreply.github.com>
2023-09-02 15:32:21 -0500

    MongoDB connected

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
 import express from "express";
+import cors from "cors"; // this allows us to communicate with front end (send data, update...)
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config(); //this allows us to config the process.env
+
+const port = process.env.PORT || 3000;
+const uri = process.env.ATLAS_URI;
+
+app.use(express.json); // a middleware that alows us to send and receive data in app
+app.use(cors());
+
+app.listen(port, (req, res) => {
+  console.log(`The server is lauched at port ${port}`);
+});
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`MongoDB connected successfully`))
+  .catch((error) => console.log(`MongoDB connected fail: `, error.message));
